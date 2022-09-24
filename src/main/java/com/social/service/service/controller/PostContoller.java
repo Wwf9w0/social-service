9d037637a659ecd67901bea23cc582a2d3bf7e9e
wwf9w0 <aliemrekaradag1@gmail.com>
2022-09-24 14:02:03 +0300

    added post controller and create endpoint

--- src/main/java/com/social/service/service/controller/PostContoller.java
+++ src/main/java/com/social/service/service/controller/PostContoller.java
@@ -1,6 +1,12 @@
 package com.social.service.service.controller;
 
+import com.social.service.service.persistence.jpa.request.CreatePostRequest;
+import com.social.service.service.service.PostService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,5 +14,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 @RequestMapping("/post")
 public class PostContoller {
+    private final PostService postService;
 
+    @PostMapping("/crete")
+    public ResponseEntity<HttpStatus> create(@RequestBody CreatePostRequest createPostRequest){
+        postService.savePost(createPostRequest);
+        return ResponseEntity.ok(HttpStatus.OK);
+    }
 }
